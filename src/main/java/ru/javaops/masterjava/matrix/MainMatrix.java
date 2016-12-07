b8fefa5db7d0c50b3edd1efa53dc394190a20284
cuidadoso <alexander_s@front.ru>
2016-12-07 23:15:58 +0200

    Prepare for HW1.

--- src/main/java/ru/javaops/masterjava/matrix/MainMatrix.java
+++ src/main/java/ru/javaops/masterjava/matrix/MainMatrix.java
@@ -1,5 +1,9 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * gkislin
  * 03.07.2016
@@ -8,16 +12,40 @@ public class MainMatrix {
     // Multiplex matrix
     private static final int MATRIX_SIZE = 1000;
     private static final int THREAD_NUMBER = 10;
+    private final static ExecutorService executor = Executors.newFixedThreadPool(MainMatrix.THREAD_NUMBER);
 
-    public static void main(String[] args) {
-        final int[][] matrixA = new int[MATRIX_SIZE][MATRIX_SIZE];
-        final int[][] matrixB = new int[MATRIX_SIZE][MATRIX_SIZE];
+    public static void main(String[] args) throws ExecutionException, InterruptedException
+    {
+        final int[][] matrixA = MatrixUtil.create(MATRIX_SIZE);
+        final int[][] matrixB = MatrixUtil.create(MATRIX_SIZE);
 
-        long start = System.currentTimeMillis();
-        final int[][] matrixC =  MatrixUtil.singleThreadMultiply(matrixA, matrixB);
-        System.out.println("Single thread multiplication time, sec: " + (System.currentTimeMillis() - start)/1000.);
+        double singleThreadSum = 0.;
+        double concurrentThreadSum = 0.;
 
-        // TODO implement parallel multiplication matrixA*matrixB
-        // TODO compare wih matrixC;
+        for (int i = 0; i < 5; i++) {
+            long start = System.currentTimeMillis();
+            final int[][] matrixC = MatrixUtil.singleThreadMultiply(matrixA, matrixB);
+            double duration = (System.currentTimeMillis() - start) / 1000.;
+            out("Single thread time, sec: %.3f", duration);
+            singleThreadSum += duration;
+
+            start = System.currentTimeMillis();
+            final int[][] concurrentMatrixC = MatrixUtil.concurrentMultiply(matrixA, matrixB, executor);
+            duration = (System.currentTimeMillis() - start) / 1000.;
+            out("Concurrent thread time, sec: %.3f", duration);
+            concurrentThreadSum += duration;
+
+            if (!MatrixUtil.compare(matrixC, concurrentMatrixC)) {
+                System.err.println("Comparison failed");
+                break;
+            }
+        }
+        executor.shutdown();
+        out("\nAverage single thread time, sec: %.3f", singleThreadSum / 5.);
+        out("Average concurrent thread time, sec: %.3f", concurrentThreadSum / 5.);
+    }
+
+    private static void out(String format, double ms) {
+        System.out.println(String.format(format, ms));
     }
 }
